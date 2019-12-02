14e8922f818aeb8e90588aa23fbe7a5c78c7e67e
judith <ceogames2014@gmail.com>
2019-12-02 05:19:13 +0100

    WIP: Comments work but not post_id is set

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
+   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+   before_action :set_post
   # GET /comments
   # GET /comments.json
   def index
@@ -24,12 +24,13 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+  #  @comment = Comment.new(comment_params)
+    @comment = current_user.comments.build(comment_params)
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        format.html { redirect_to post_path(id: @comment.post_id), notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: post_path(id: @comment.post_id) }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -62,13 +63,17 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  #  Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
     end
 
+    def set_post
+      @post = Post.find(1)
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:title, :content, :user_id, :post_id)
+      params.require(:comment).permit(:title, :content, :post_id, :user_id)
     end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,9 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.includes(:comments).find(params[:id])
+  #  @comment = Comment.new
+
   end
 
   # GET /posts/new
